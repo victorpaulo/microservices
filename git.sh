436b0a1d3784ddbac3ccf284775656ff7513ab24
Victor Almeida <victorp@victorp.home>
2019-04-07 10:23:44 +0100

    adding

--- git.sh
+++ git.sh
@@ -2,6 +2,10 @@
 
 doGit() {
     local comment=$1
+    if [ -z $comment ]; then
+	echo "Please provide a comment"
+	exit 1
+    fi
     git add .
     git commit -m $comment
     git push origin master
