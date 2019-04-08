52a135a79a3776aeb0d16247698c369cad5b609d
Victor Almeida <victorp@victorp.home>
2019-04-08 21:44:03 +0100

    fix

--- git.sh
+++ git.sh
@@ -3,8 +3,8 @@
 doGit() {
     local comment=$1
     if [ -z $comment ]; then
-	echo "Please provide a comment"
-	exit 1
+        echo "Please provide a comment"
+        exit 1
     fi
     git add .
     git commit -m $comment
