8ce96032bf6bd4bc00256ffb56fc779f5c10500b
Victor <victor.stf@gmail.com>
2020-07-25 16:06:09 +0100

    adding app3

---
+++ app3/src/main/java/com/example/main/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.main;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+	
+}
